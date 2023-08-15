11a4bdd5e43492271d42d8edb164ac70409f9ef7
janusec <support@janusec.com>
2023-08-15 21:16:09 +0800

    add close source description

---
+++ janucat.go
@@ -0,0 +1,14 @@
+/*
+ * @Copyright Reserved By JANUSEC (https://www.janusec.com/).
+ * @Date: 2023-08-12 15:28:19
+ */
+
+ package main
+
+ import (
+	 "fmt"
+ )
+  
+ func main() {
+	fmt.Println("JANUCAT is a closed source data privacy solution aimed at building compliance, accountability, and transparency")
+ }
